9828bb905573fee2a501da6899c2154a2c19dcf2
Gurleen Dhaliwal <gurleendhaliwal86@gmail.com>
2024-10-12 15:00:22 -0700

    Initial Commit

---
+++ PhotoEdit/PhotoEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditApp.swift
+//  PhotoEdit
+//
+//  Created by Gurleen Dhaliwal on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
